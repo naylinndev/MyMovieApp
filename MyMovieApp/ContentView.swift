389b550a65d026845d741dddeac9278da94b5270
Nay Linn <naylinn@weeswares.com>
2025-08-16 19:20:21 +0630

    Initial Commit

---
+++ MyMovieApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMovieApp
+//
+//  Created by Nay Lin on 2025/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyMovieApp/MyMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieAppApp.swift
+//  MyMovieApp
+//
+//  Created by Nay Lin on 2025/08/16.
+//
+
+import SwiftUI
+
+@main
+struct MyMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
